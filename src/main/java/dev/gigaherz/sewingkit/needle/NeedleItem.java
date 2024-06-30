7436b633335b7060605d0ca2044ba9abc243c6ca
gigaherz <gigaherz@gmail.com>
2024-06-30 19:26:25 +0200

    Tmp

--- src/main/java/dev/gigaherz/sewingkit/needle/NeedleItem.java
+++ src/main/java/dev/gigaherz/sewingkit/needle/NeedleItem.java
@@ -2,22 +2,15 @@ package dev.gigaherz.sewingkit.needle;
 
 import dev.gigaherz.sewingkit.SewingKitMod;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.Registry;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.neoforged.neoforge.common.ToolAction;
 
-import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Set;
 
 
 public class NeedleItem extends DiggerItem
